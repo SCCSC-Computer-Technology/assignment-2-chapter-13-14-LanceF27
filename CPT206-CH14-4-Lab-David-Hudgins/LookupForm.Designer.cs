8accc9892d399517b361938ff2567a70a67b584f
LanceF27 <155991097+LanceF27@users.noreply.github.com>
2024-01-22 21:01:14 -0500

    Add files via upload

---
+++ CPT206-CH14-4-Lab-David-Hudgins/LookupForm.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace CPT206_CH14_4_Lab_David_Hudgins
+{
+    partial class LookupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.productNumberTextBox = new System.Windows.Forms.TextBox();
+            this.productNumberSearchButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionSearchButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.productDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.productNumberTextBox);
+            this.groupBox1.Controls.Add(this.productNumberSearchButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.descriptionTextBox);
+            this.groupBox1.Controls.Add(this.descriptionSearchButton);
+            this.groupBox1.Location = new System.Drawing.Point(35, 33);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox1.Size = new System.Drawing.Size(873, 281);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product Lookup";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(72, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Search By Product Number";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // productNumberTextBox
+            // 
+            this.productNumberTextBox.Location = new System.Drawing.Point(56, 105);
+            this.productNumberTextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.productNumberTextBox.Name = "productNumberTextBox";
+            this.productNumberTextBox.Size = new System.Drawing.Size(328, 35);
+            this.productNumberTextBox.TabIndex = 13;
+            // 
+            // productNumberSearchButton
+            // 
+            this.productNumberSearchButton.Location = new System.Drawing.Point(157, 161);
+            this.productNumberSearchButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.productNumberSearchButton.Name = "productNumberSearchButton";
+            this.productNumberSearchButton.Size = new System.Drawing.Size(129, 62);
+            this.productNumberSearchButton.TabIndex = 12;
+            this.productNumberSearchButton.Text = "Search";
+            this.productNumberSearchButton.UseVisualStyleBackColor = true;
+            this.productNumberSearchButton.Click += new System.EventHandler(this.productNumberSearchButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(528, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 26);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Search By Description";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(485, 105);
+            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(328, 35);
+            this.descriptionTextBox.TabIndex = 10;
+            // 
+            // descriptionSearchButton
+            // 
+            this.descriptionSearchButton.Location = new System.Drawing.Point(586, 161);
+            this.descriptionSearchButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.descriptionSearchButton.Name = "descriptionSearchButton";
+            this.descriptionSearchButton.Size = new System.Drawing.Size(129, 62);
+            this.descriptionSearchButton.TabIndex = 9;
+            this.descriptionSearchButton.Text = "Search";
+            this.descriptionSearchButton.UseVisualStyleBackColor = true;
+            this.descriptionSearchButton.Click += new System.EventHandler(this.descriptionSearchButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(358, 359);
+            this.showAllButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(226, 62);
+            this.showAllButton.TabIndex = 16;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(779, 359);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(129, 62);
+            this.exitButton.TabIndex = 15;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(35, 359);
+            this.resetButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(129, 62);
+            this.resetButton.TabIndex = 14;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // productDataGridView
+            // 
+            this.productDataGridView.AutoGenerateColumns = false;
+            this.productDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.productDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.productDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.productDataGridView.DataSource = this.productBindingSource;
+            this.productDataGridView.Location = new System.Drawing.Point(24, 530);
+            this.productDataGridView.Name = "productDataGridView";
+            this.productDataGridView.RowHeadersWidth = 62;
+            this.productDataGridView.RowTemplate.Height = 28;
+            this.productDataGridView.Size = new System.Drawing.Size(894, 453);
+            this.productDataGridView.TabIndex = 17;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Product_Number";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Product_Number";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Description";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Description";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Units_On_Hand";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Units_On_Hand";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Price";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Price";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataSource = typeof(CPT206_CH14_4_Lab_David_Hudgins.Product);
+            // 
+            // LookupForm
+            // 
+            this.AcceptButton = this.productNumberSearchButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(942, 1007);
+            this.Controls.Add(this.productDataGridView);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "LookupForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DavidHudgins-CH14.4";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox productNumberTextBox;
+        private System.Windows.Forms.Button productNumberSearchButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Button descriptionSearchButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private System.Windows.Forms.DataGridView productDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
---
+++ CPT206-CH14-4-Lab-David-Hudgins/LookupForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPT206_CH14_4_Lab_David_Hudgins
+{
+    public partial class LookupForm : Form
+    {
+        public LookupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            productNumberTextBox.Clear();
+            descriptionTextBox.Clear();
+            productDataGridView.DataSource = "";
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            ProductDBDataContext db = new ProductDBDataContext();       //Takes LINQ to SQL Class and creates object
+
+            var results = from product in db.Products           //Selects all rows
+                          select product;
+
+            productDataGridView.DataSource = results;       //Fills Gridview table
+        }
+
+        private void productNumberSearchButton_Click(object sender, EventArgs e)
+        {
+            if(productNumberTextBox.Text != "")
+            {
+                productDataGridView.DataSource = "";
+
+                ProductDBDataContext db = new ProductDBDataContext();
+
+                var results = from product in db.Products
+                              where product.Product_Number.Equals(productNumberTextBox.Text)        //Looks for matching product number with textbox entry
+                              select product;
+
+                productDataGridView.DataSource = results;
+            }
+            else
+            {
+                MessageBox.Show("Please Enter A Product Number");
+            }
+
+        }
+
+        private void descriptionSearchButton_Click(object sender, EventArgs e)
+        {
+            if(descriptionTextBox.Text != "")
+            {
+                productDataGridView.DataSource = "";
+
+                ProductDBDataContext db = new ProductDBDataContext();
+
+                var results = from product in db.Products
+                              where product.Description.Contains(descriptionTextBox.Text)       //Finds all rows with descriptions containing textbox entry
+                              select product;
+
+                productDataGridView.DataSource = results;
+            }
+            else
+            {
+                MessageBox.Show("Please Enter A Description");
+            }
+        }
+    }
+}
